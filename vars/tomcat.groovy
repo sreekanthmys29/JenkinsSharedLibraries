d9b9df6e2d4e257b000e2abf2be5d8da0e1313f4
sreekanthmys29 <110405027+sreekanthmys29@users.noreply.github.com>
2025-05-27 10:38:54 +0530

    Update tomcat.groovy

--- vars/tomcat.groovy
+++ vars/tomcat.groovy
@@ -1,1 +1,16 @@
+def call(){
 
+  
+      withCredentials([usernamePassword(credentialsId: 'tomcat', passwordVariable: 'password', usernameVariable: 'username')]) {
+
+       // Upload the WAR file to the remote server (e.g., Tomcat manager)
+    sh """
+      curl -v --upload-file target/spring3-mvc-maven-xml-hello-world.war \
+           -u ${username}:${password} \
+           http://34.172.209.37:8085/manager/text/deploy?path=/spring3&update=true
+    """
+    
+      }
+
+
+}
