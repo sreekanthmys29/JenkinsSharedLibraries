7ef08ef2e1bcb40e5abfe856456717c558bd5f90
sreekanthmys29 <110405027+sreekanthmys29@users.noreply.github.com>
2025-05-27 12:46:08 +0530

    Update git.groovy

--- vars/git.groovy
+++ vars/git.groovy
@@ -1,12 +1,1 @@
-// vars/buildSpringApp.groovy
 
-def call(Map params = [:]) {
-    def branch = params.get('branch', 'master')
-    def url = params.get('url', '')
-    def credentialsId = params.get('credentialsId', '')
-
-  git branch: branch, credentialsId: credentialsId, url: url
-     
-   
-   
-}
