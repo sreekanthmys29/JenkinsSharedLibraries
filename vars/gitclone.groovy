7d41d3ca74f4bfd3af7c5d152e889ab1c39f7798
sreekanthmys29 <110405027+sreekanthmys29@users.noreply.github.com>
2025-05-27 11:58:21 +0530

    Update gitclone.groovy

--- vars/gitclone.groovy
+++ vars/gitclone.groovy
@@ -1,1 +1,8 @@
 
+def call(String branch , String credentialsId, String url) {
+   echo "branch -----${branch}"
+    echo "credentialsId -----${credentialsId}"
+    echo "url -----${url}"
+     echo "git clone"
+    git branch: branch, credentialsId: credentialsId, url: url
+}
