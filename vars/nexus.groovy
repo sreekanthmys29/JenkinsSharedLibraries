42e672050b90cff7be31e49bb0589a2dfc21aef2
sreekanthmys29 <110405027+sreekanthmys29@users.noreply.github.com>
2025-05-27 13:39:01 +0530

    Update nexus.groovy

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -1,12 +1,19 @@
 def call() {
-    echo " Install Jenkins  Plugin  Pipeline Maven Integration..."
+    //Install jenkins  Pipeline Maven Integration Plugin
+    //echo " Install Jenkins  Plugin  Pipeline Maven Integration..."
+   // def pom = readMavenPom file: 'pom.xml'
+   // echo "   POM: {pom} "
+   // echo " - GroupId: ${pom.groupId}"
+    //echo " - ArtifactId: ${pom.artifactId}"
+   // echo " - Version: ${pom.version}"
+   // echo " - Packaging: ${pom.packaging}"
+    
+        //If not Plugin , You can parse the pom.xml manually in Groovy
+        def pomXml = readFile('pom.xml')
+        def pom = new XmlSlurper().parseText(pomXml)
+        echo "GroupId: ${pom.groupId.text()}"
+        echo "ArtifactId: ${pom.artifactId.text()}"
+        echo "Version: ${pom.version.text()}"
 
-    // Read and parse pom.xml
-    def pom = readMavenPom file: 'pom.xml'
-
-    echo "   POM: {pom} "
-    echo " - GroupId: ${pom.groupId}"
-    echo " - ArtifactId: ${pom.artifactId}"
-    echo " - Version: ${pom.version}"
-    echo " - Packaging: ${pom.packaging}"
+    
 }
