cca0d208762d54d71ea9e27e5c204c4c1418448b
sreekanthmys29 <110405027+sreekanthmys29@users.noreply.github.com>
2025-05-27 13:31:13 +0530

    Update nexus.groovy

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -1,7 +1,10 @@
 def call(){
 
      echo "Nexus deployment"
-       def pom = readMavenPom file: 'pom.xml'
-       echo "Parsed POM - GroupId: ${pom.groupId}, ArtifactId: ${pom.artifactId}, Version: ${pom.version}"
+
+def pom = readMavenPom file: 'pom.xml'
+
+echo "Parsed POM - GroupId: ${pom.groupId}, ArtifactId: ${pom.artifactId}, Version: ${pom.version}"
+
 
 }
