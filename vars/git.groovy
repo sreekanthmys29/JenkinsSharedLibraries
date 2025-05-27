b08a33437bf49aa817457acb2b41b2d281bb98f6
sreekanthmys29 <110405027+sreekanthmys29@users.noreply.github.com>
2025-05-27 11:02:09 +0530

    Update git.groovy

--- vars/git.groovy
+++ vars/git.groovy
@@ -1,3 +1,4 @@
 def call(){
+       echo "Invoke GIt Groovy------"
        git branch: 'main', credentialsId: 'jenkinsGit', url: 'https://github.com/sreekanthmys29/spring3-mvc-maven-xml-hello-world.git'
 }
