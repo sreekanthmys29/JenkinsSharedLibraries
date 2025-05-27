f02c3c73869d17fcf05fbb5ee1aed4694a1878b7
sreekanthmys29 <110405027+sreekanthmys29@users.noreply.github.com>
2025-05-27 10:24:24 +0530

    Update sonar.groovy

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -1,1 +1,9 @@
+def call(){
 
+ sh 'mvn clean verify sonar:sonar \
+  -Dsonar.projectKey=spring3-mvc-maven-xml-hello-world \
+  -Dsonar.host.url=http://34.172.209.37:9000 \
+  -Dsonar.login=sqp_1b0b1c182e2397e490deb5f55c06987ce66fb757'
+  
+
+}
