997d309dbd6beca53644221261db94294e8d8a03
sreekanthmys29 <110405027+sreekanthmys29@users.noreply.github.com>
2025-05-27 12:41:28 +0530

    Update git.groovy

--- vars/git.groovy
+++ vars/git.groovy
@@ -5,7 +5,7 @@ def call(Map params = [:]) {
     def url = params.get('url', '')
     def credentialsId = params.get('credentialsId', '')
 
-  git branch: 'master', credentialsId: 'jenkinsGit', url: 'https://github.com/sreekanthmys29/spring3-mvc-maven-xml-hello-world.git'
+  git branch: branch, credentialsId: credentialsId, url: url
      
    
    
