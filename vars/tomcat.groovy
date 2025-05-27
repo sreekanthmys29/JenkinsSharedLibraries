614e8c378926c7b0665d8f2e4a43503d000265eb
sreekanthmys29 <110405027+sreekanthmys29@users.noreply.github.com>
2025-05-27 13:20:32 +0530

    Update tomcat.groovy

--- vars/tomcat.groovy
+++ vars/tomcat.groovy
@@ -1,22 +1,25 @@
-def call(){
-       echo "Workspace: ${env.WORKSPACE}"
-       echo "Job Name: ${env.JOB_NAME}"
+def call() {
+    echo "Workspace: ${env.WORKSPACE}"
+    echo "Job Name: ${env.JOB_NAME}"
 
-       def WAR_FILE=$(find target -name "*.war" | head -n 1)
-       echo "WAR_FILE : ${${env.JOB_NAME}}"
-  
-      withCredentials([usernamePassword(credentialsId: 'tomcat', passwordVariable: 'password', usernameVariable: 'username')]) {
+    // Find WAR file path using shell, and store it in a Groovy variable
+    def warFile = sh(
+        script: 'find target -name "*.war" | head -n 1',
+        returnStdout: true
+    ).trim()
 
-       // Upload the WAR file to the remote server (e.g., Tomcat manager) 
-        // --upload-file target/spring3-mvc-maven-xml-hello-world.war  or -T  target/spring3-mvc-maven-xml-hello-world.war
-    sh """
-       
-        curl -v --upload-file "$WAR_FILE" \
-          -u admin:${TOMCAT_PASSWORD} \
-          "http://34.172.209.37:8085/manager/text/deploy?path=/spring3&update=true"
-    """
-    
-      }
+    echo "WAR File: ${warFile}"
 
+    withCredentials([usernamePassword(
+        credentialsId: 'tomcat',
+        usernameVariable: 'USERNAME',
+        passwordVariable: 'PASSWORD'
+    )]) {
 
+        sh """
+            curl -v --upload-file "${warFile}" \\
+              -u ${USERNAME}:${PASSWORD} \\
+              "http://34.172.209.37:8085/manager/text/deploy?path=/spring3&update=true"
+        """
+    }
 }
