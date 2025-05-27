ec9551d40fc2d4e1238a88e995c91cf13ebcb98b
sreekanthmys29 <110405027+sreekanthmys29@users.noreply.github.com>
2025-05-27 13:11:05 +0530

    Update tomcat.groovy

--- vars/tomcat.groovy
+++ vars/tomcat.groovy
@@ -6,7 +6,7 @@ def call(){
        // Upload the WAR file to the remote server (e.g., Tomcat manager) 
         // --upload-file target/spring3-mvc-maven-xml-hello-world.war  or -T  target/spring3-mvc-maven-xml-hello-world.war
     sh """
-      curl -v --upload-file target/spring3-mvc-maven-xml-hello-world.war \
+      curl -v --upload-file **/target/.war \
            -u ${username}:${password} \
            http://34.172.209.37:8085/manager/text/deploy?path=/spring3&update=true
     """
