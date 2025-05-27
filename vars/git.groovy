7a83e97eacce8b6001d0d2ef8b454450388d3504
sreekanthmys29 <110405027+sreekanthmys29@users.noreply.github.com>
2025-05-27 12:34:19 +0530

    Update git.groovy

--- vars/git.groovy
+++ vars/git.groovy
@@ -1,26 +1,12 @@
+// vars/buildSpringApp.groovy
+
 def call(Map params = [:]) {
-    // Extract parameters with defaults
     def branch = params.get('branch', 'master')
     def repoUrl = params.get('repoUrl', '')
     def credentialsId = params.get('credentialsId', '')
 
-    pipeline {
-        agent any
-        tools {
-            git 'Default'
-            maven 'mavenLocal'
-        }
-        stages {
-            stage('Checkout code') {
-                steps {
-                    git branch: branch, credentialsId: credentialsId, url: repoUrl
-                }
-            }
-            stage('Maven build') {
-                steps {
-                    sh 'mvn clean package'
-                }
-            }
-        }
-    }
+  
+        git branch: branch, credentialsId: credentialsId, url: repoUrl
+   
+   
 }
