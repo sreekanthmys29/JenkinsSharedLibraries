6e8eece6e3a4759d6b9177584e20af5a1b4e5e2e
sreekanthmys29 <110405027+sreekanthmys29@users.noreply.github.com>
2025-05-27 12:28:52 +0530

    Update git.groovy

--- vars/git.groovy
+++ vars/git.groovy
@@ -1,1 +1,21 @@
-
+def call(String branch = 'master', String repoUrl, String credentialsId) {
+    pipeline {
+        agent any
+        tools {
+            git 'Default'
+            maven 'mavenLocal'
+        }
+        stages {
+            stage('Checkout code') {
+                steps {
+                    git branch: branch, credentialsId: credentialsId, url: repoUrl
+                }
+            }
+            stage('Maven build') {
+                steps {
+                    sh 'mvn clean package'
+                }
+            }
+        }
+    }
+}
